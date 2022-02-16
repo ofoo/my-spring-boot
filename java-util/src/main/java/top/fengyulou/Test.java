4eeada467ed9c96cc15205e9375a5a0d6eb5909f
仲裁圣骑 <guochao@nxb100.com>
2022-02-16 18:32:22 +0800

    提交代码

---
+++ java-util/src/main/java/top/fengyulou/Test.java
@@ -0,0 +1,12 @@
+package top.fengyulou;
+
+/**
+ * @author guochao
+ * @version 1.0
+ * @date 2022/2/16 18:15
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.getProperty("user.dir");
+    }
+}
