576e41efc1a871c6fa8d0fca82461e1b3d1d5228
guochao <guochao@nxb100.com>
2022-10-07 17:24:08 +0800

    1.增加 Amaze UI 后台管理模板 2.增加分表练习项目

---
+++ sub-table-spring-boot/src/main/java/com/example/amazeui/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.example.amazeui.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author guochao
+ * @version 1.0
+ * @date 2022/10/7 14:04
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/")
+    public String adminLogin(){
+        return "admin/admin-login";
+    }
+
+    @RequestMapping("/admin/list")
+    public String adminList(){
+        return "admin/admin-list";
+    }
+
+    @RequestMapping("/admin/save")
+    public String adminSave(){
+        return "admin/admin-save";
+    }
+}
